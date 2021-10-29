15a23f7ecd3ead813f00f728c0b959bec65ad689
Shreeya Dhole <55687612+Shreeya18@users.noreply.github.com>
2021-10-29 16:11:02 +0530

    space updated

--- app/src/main/java/com/androidp/taskmanager/MainActivity.java
+++ app/src/main/java/com/androidp/taskmanager/MainActivity.java
@@ -19,12 +19,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
-
-
-
-        new Handler().postDelayed(new Runnable() {
+            new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
 
